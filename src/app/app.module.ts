9feeb5297ef12bf88507a334c45c1bde81787334
Ismoil Tohirov <ismoiltohirov55@gmail.com>
2024-04-17 09:23:18 +0500

    get started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CreateComponent } from './components/create/create.component';
+import { GetAllComponent } from './components/get-all/get-all.component';
+import { GetByIdComponent } from './components/get-by-id/get-by-id.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UpdateComponent } from './components/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    GetAllComponent,
+    GetByIdComponent,
+    DeleteComponent,
+    NavbarComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
